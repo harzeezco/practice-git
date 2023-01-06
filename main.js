21702be3222884b033b360576fe4817917f90925
Wakil Azeez <wakilabdulazeez@gmail.com>
2023-01-06 13:19:40 +0100

    Added new branch and some changes

---
+++ main.js
@@ -0,0 +1,7 @@
+const prac = document.querySelector('.prac')
+function name() {
+    document.body.style.backgroundColor = '#222';
+    document.body.style.color = '#fff';
+     prac.style.color = 'blue'
+}
+name()
